91d1696df2ef7ccd16b0423d8e654c5e523131c4
adria <adriancarrerolucha93@gmail.com>
2026-02-24 14:35:00 +0100

    Ejercicios de diccionarios

---
+++ dictFruit.py
@@ -0,0 +1,19 @@
+#Copia el diccionario a un nuevo diccionario
+fruits={1:'apples',2:'bananas',3:'pears',4:'strawberries',5:'watermelons'}
+fruits_copy=fruits.copy()
+#Muestra una lista de elementos clave-valor de fruits_copy
+print(list(fruits_copy.items()))
+#Muestra una lista de las claves
+print(list(fruits_copy.keys()))
+#Muestra una lista de los valores
+print(list(fruits_copy.values()))
+#Muestra el valor de la clave 3
+print(fruits_copy.get(3))
+#Actualiza el valor de clave 4
+fruits_copy.update({4:'blueberries'})
+#Añade un nuevo elemento
+fruits_copy.setdefault(6,'pineapples')
+#Elimina el elemento con la clave 5
+fruits_copy.pop(5)
+#Muestra los elementos de la cpia del diccionario
+print(list(fruits_copy.items()))
---
+++ dictInventory.py
@@ -0,0 +1,26 @@
+#Crea un diccionario que represente productos y cantidades
+inventory1={
+    "chocolate":3,"biscuits":6,"juice":2,"chips":5
+}
+#Añade un nuevo elemento y muestralo
+inventory1["apples"]=8
+print(inventory1)
+#Edita el valor de juice para que sea 1
+inventory1["juice"]=1
+#Elimina el elemento chocolate y muestralo
+del inventory1["chocolate"]
+print(inventory1)
+#Crea un nuevo diccionario
+inventory2={"chocolate":6,"biscuits":4,"milk":7,"oranges":3}
+#Crea un nuevo diccionario que anide los dos anteriores con las claves branch1 y branch2
+inventory={"branch1":inventory1, "branch2":inventory2}
+#Muestra el nuevo diccionario
+print(inventory)
+#Muestra el valor de biscuits en branch2
+print(inventory["branch2"]["biscuits"])
+#Crea y muestra un nuevo diccionario llamado codes para asignar codigo a los productos de la siguiente lista utilizando la enumeración
+products={"cholotate", "biscuits","juice","milk","apples","chips","orange"}
+lang_enum=enumerate(products)
+
+dict_lang=dict((i,j)for i,j in lang_enum)
+print(dict_lang)
